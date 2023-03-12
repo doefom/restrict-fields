6b95ebbb7430a675c8f34ad31bb4d6ba6afba2e0
doefom <dom.mueller94@gmail.com>
2023-03-12 19:41:50 +0100

    Add BootTest

--- tests/Feature/BootTest.php
+++ tests/Feature/BootTest.php
@@ -10,6 +10,13 @@ class BootTest extends TestCase
     /** @test */
     public function is_applied_to_all_field_types()
     {
-        $this->assertTrue(true);
+        $fieldTypes = collect(config('restrict_fields.field_types'));
+        $isAppliedToAllFieldTypes = $fieldTypes->every(function ($fieldType) {
+            $ft = new $fieldType();
+            // Every field must have one item with the handle 'restrictions'
+            return $ft->configFields()->items()->some(fn($item) => $item['handle'] === 'restrictions');
+        });
+
+        $this->assertTrue($isAppliedToAllFieldTypes);
     }
 }
