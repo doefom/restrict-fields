13f87fc521220647da03416f2a33a272056b9c3a
dominikmuller <dom.mueller94@gmail.com>
2023-03-12 08:33:03 +0100

    WIP

--- src/Listeners/RestrictFields.php
+++ src/Listeners/RestrictFields.php
@@ -2,8 +2,10 @@
 
 namespace Doefom\RestrictFields\Listeners;
 
+use Illuminate\Support\Collection;
 use Statamic\Events\EntryBlueprintFound;
 use Statamic\Facades\User;
+use Statamic\Fields\Blueprint;
 use Statamic\Support\Arr;
 
 class RestrictFields
@@ -26,25 +28,54 @@ class RestrictFields
      */
     public function handle(EntryBlueprintFound $event)
     {
-        $items = $event->blueprint->fields()->items();
+        $itemsWithRestrictionForCurrentUser = $this->getFieldsWithRestrictionsForCurrentUser($event->blueprint);
+        $this->applyRestrictions($itemsWithRestrictionForCurrentUser, $event->blueprint);
+    }
+
+    private function getFieldsWithRestrictionsForCurrentUser(Blueprint $blueprint): Collection
+    {
+        return $blueprint->fields()->items()->filter(function (array $item) {
+            return Arr::has($item, 'field.restrictions') && $this->hasRestrictionsForUser($item, User::current());
+        });
+    }
+
+    private function hasRestrictionsForUser(array $item, \Statamic\Contracts\Auth\User $user): bool
+    {
+        $restrictions = Arr::get($item, 'field.restrictions');
+        $restrictedRoles = collect($restrictions)
+            ->pluck('restrict_for_roles')
+            ->flatten();
+        // Return true if user has at least on of the roles restricted. Else return false.
+        return $restrictedRoles->some(function ($role) use ($user) {
+            return $user->hasRole($role);
+        });
+    }
+
+    private function applyRestrictions(Collection $items, Blueprint $blueprint)
+    {
         foreach ($items as $item) {
-            // Check if field has 'hide_for_roles' config
-            $hideForRoles = Arr::get($item, 'field.hide_for_roles');
-            if (!$hideForRoles) {
-                continue;
+            $restrictions = Arr::get($item, 'field.restrictions');
+            foreach ($restrictions as $restriction) {
+                $type = Arr::get($restriction, 'restrict_visibility');
+                if ($type === 'read_only') {
+                    $this->applyReadOnlyRestriction($item, $blueprint);
+                }
+                if ($type === 'hidden') {
+                    $this->applyHiddenRestriction($item, $blueprint);
+                }
             }
-
-            // Check if field should be hidden for the current user
-            $hideForCurrentUser = collect($hideForRoles)
-                ->contains(function (string $role) {
-                    return User::current()->hasRole($role);
-                });
-            if (!$hideForCurrentUser) {
-                continue;
-            }
-
-            // If field should be hidden, remove from the blueprint.
-            $event->blueprint->removeField(Arr::get($item, 'handle'));
         }
     }
+
+    private function applyReadOnlyRestriction(array $item, Blueprint $blueprint)
+    {
+        // TODO: Continue here. Seems like code runs up to this point
+        return;
+    }
+
+    private function applyHiddenRestriction(array $item, Blueprint $blueprint)
+    {
+        $blueprint->removeField(Arr::get($item, 'handle'));
+    }
+
 }
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -20,13 +20,86 @@ class ServiceProvider extends AddonServiceProvider
 
         $roles = Role::all()->map(fn(\Statamic\Contracts\Auth\Role $role) => $role->handle());
 
-        // One field...
-        Text::appendConfigField('hide_for_roles', [
-            'type' => 'select',
-            'display' => 'Hide for Roles',
-            'options' => $roles,
-            'multiple' => true,
-            'clearable' => true,
+//        Text::appendConfigField('restrict_visibility', [
+//            'type' => 'select',
+//            'display' => 'Restrict Visibility',
+//            'options' => ['read_only', 'hidden'],
+//            'multiple' => false,
+//            'clearable' => true,
+//            'width' => 33
+//        ]);
+//
+//        Text::appendConfigField('restrict_for_roles', [
+//            'type' => 'select',
+//            'display' => 'Restrict for Roles',
+//            'options' => $roles,
+//            'multiple' => true,
+//            'clearable' => true,
+//            'width' => 66
+//        ]);
+
+        Text::appendConfigField('restrictions', [
+            'collapse' => false,
+            'previews' => true,
+            'sets' => [
+                'restriction' => [
+                    'display' => 'restriction',
+                    'fields' => [
+                        [
+                            'handle' => 'restrict_visibility',
+                            'field' => [
+                                'options' => [
+                                    'read_only' => 'Read Only',
+                                    'hidden' => 'Hidden',
+                                ],
+                                'multiple' => false,
+                                'max_items' => 1,
+                                'clearable' => true,
+                                'searchable' => false,
+                                'taggable' => false,
+                                'push_tags' => false,
+                                'cast_booleans' => false,
+                                'display' => 'Restrict Visibility',
+                                'type' => 'select',
+                                'icon' => 'select',
+                                'instructions' => 'Select how you would like to restrict the visibility of this field for certain roles.',
+                                'width' => 50,
+                                'listable' => 'hidden',
+                                'instructions_position' => 'above',
+                                'visibility' => 'visible',
+                                'required' => true,
+                            ],
+                        ],
+                        [
+                            'handle' => 'restrict_for_roles',
+                            'field' => [
+                                'options' => $roles, // TODO: Use handle and label for nicer display options
+                                'multiple' => true,
+                                'clearable' => true,
+                                'searchable' => true,
+                                'taggable' => false,
+                                'push_tags' => false,
+                                'cast_booleans' => false,
+                                'display' => 'Restrict for Roles',
+                                'type' => 'select',
+                                'icon' => 'select',
+                                'instructions' => 'The roles to apply the restriction to.',
+                                'width' => 50,
+                                'listable' => 'hidden',
+                                'instructions_position' => 'above',
+                                'visibility' => 'visible',
+                                'required' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'display' => 'Restrictions',
+            'type' => 'replicator',
+            'icon' => 'replicator',
+            'listable' => 'hidden',
+            'instructions_position' => 'above',
+            'visibility' => 'visible',
         ]);
 
     }
